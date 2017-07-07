d45ff40c0d7e5049c8f89bb9f55b11a2636613ce
carlosdvp <carlosdvp@yahoo.com>
2017-07-07 10:19:18 -0400

    added new page template for projects and completed the image gallery for homepage using masonry js, also fixed collapsed navbar to close when a link is clicked on

--- js/apocaliptica.js
+++ js/apocaliptica.js
@@ -1,1 +1,16 @@
-//$('body').scrollspy({ target: '#top-navbar' })
+
+//masonry js initializer
+$('.grid').masonry({
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  percentPosition: true,
+  horizontalOrder: true
+});
+
+// to collapse the navbar when I click on one of the links
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
+
+
+// to clear the modal form input fields when it is closed ---------------

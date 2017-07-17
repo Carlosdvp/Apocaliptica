32469eaa06bbb84561182943880364e131431847
carlosdvp <carlosdvp@yahoo.com>
2017-07-16 20:32:19 -0400

    Created a new project page for CAsa Sur, added to main css and js files, made some changes to the jquery cdn on index.html

--- js/apocaliptica.js
+++ js/apocaliptica.js
@@ -9,11 +9,53 @@ $('.grid').masonry({
 
 // to collapse the navbar when I click on one of the links
 $('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
+  $('.navbar-collapse').collapse('hide');
 });
+
+//slick plubin initializer and options
+$(`.slickness`).slick({
+  autoplay: true,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  dots: true,
+  arrows: false,
+  infinite: true,
+  swipeToSlide: true,
+  responsive: [
+    {
+      breakpoint: 740,
+      settings: {
+        slidesToShow: 1,
+        infinite: true,
+        dots: true,
+        arrows: false
+      }
+    }
+  ]
+});
+
+// to initialize Tooltips for each of the images on the Main Page picture Gallery
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip({
+    container: `body`,
+    placement: `bottom`,
+    trigger: `hover`,
+    offset: `50 0`,
+    animation: false
+  })
+})
+// there has to be a way to customize the color of the text inside the tooltip. I think that this can be done either with CSS, or by using the HTML option for the tooltip combined with the text-danger Class for <p> elements.
+
+// Lightbox options
+lightbox.option({
+  albumLabel:  "Imágen %1 de %2"
+})
+
 
 
 // I need to clear the modal form input fields when it is closed, that is when either of the btn's are clicked on or the modal is closed       ---------------
+
+
 
 
 
